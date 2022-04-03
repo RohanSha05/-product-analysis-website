e39e181cc10a3161c879b39c158abe78eecda446
RohanSha05 <rhnshariar@gmail.com>
2022-04-04 00:04:12 +0600

    headerAdded

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div className='flex justify-center text-3xl m-6'>
+            <CustomLink className='mx-6' to='/'>HOME</CustomLink>
+            <CustomLink className='mx-6' to='/reviews'>REVIEWS</CustomLink>
+            <CustomLink className='mx-6' to='/dashboard'>DASHBOARD</CustomLink>
+            <CustomLink className='mx-6' to='/blogs'>BLOGS</CustomLink>
+            <CustomLink className='mx-6' to='/about'>ABOUT</CustomLink>
+        </div>
+    );
+};
+
+export default Header;
