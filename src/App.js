bc079ab87a5eceafb0222a8a9e368455c8a6c88a
RohanSha05 <rhnshariar@gmail.com>
2022-04-04 21:01:11 +0600

    not found added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Reviews from './components/Reviews/Reviews';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img className='m-52 mx-auto' src="https://th.bing.com/th/id/OIP.gk7ERwhFPqN0Rbl3kQ-pJwHaEK?pid=ImgDet&rs=1" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
