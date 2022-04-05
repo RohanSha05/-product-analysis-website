7cc6edc39441a21909b3302e97967637ba98495c
RohanSha05 <rhnshariar@gmail.com>
2022-04-05 21:28:20 +0600

    lastCommit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Reviews from './components/Reviews/Reviews';
 import NotFound from './components/NotFound/NotFound';
 import Charts from './components/Charts/Charts';
 import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path="/dashboard" element={<Charts></Charts>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>Contact Us: rohanrafat35@gmail.com</h1>
+            <h1 className='text-3xl'>Contact Us: rohanrafat35@gmail.com</h1>
         </div>
     );
 };
