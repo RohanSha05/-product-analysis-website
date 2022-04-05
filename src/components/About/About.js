96c0c333536ba3e03452616a70b0ba888e4ff6c3
RohanSha05 <rhnshariar@gmail.com>
2022-04-05 21:22:36 +0600

    final commit

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Contact Us: rohanrafat35@gmail.com</h1>
+        </div>
+    );
+};
+
+export default About;
